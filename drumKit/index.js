fce31d67f71521513ebcddb6d8cd1fc281b966fe
Faria Begum Riya <riya09@users.noreply.github.com>
2018-01-08 23:41:54 +0600

    fixed keypress bug

--- drumKit/index.js
+++ drumKit/index.js
@@ -1,15 +1,35 @@
+var pressed={
+    49:false,
+    50:false,
+    51:false,
+    52:false,
+    53:false,
+    54:false,
+    55:false,
+    56:false,
+    57:false,
+}
 function playSound(e){
+    //get the data-key attribute which is same as the keycode of the key defined
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if(!audio)return;
-    audio.currentTime=0;
-    audio.play();
-    key.classList.add('playing');
+    if(!audio)return;//return if other keys are pressed
+    if(!pressed[key.dataset.key]){//keydown fired only once
+        audio.currentTime=0;
+        audio.play();
+        pressed[key.dataset.key]=true;
+        key.classList.add('playing');//show transform effect
+    }
 }
+//transition is removed to change back the style
 function removeTransition(e){
     if(e.propertyName!=='transform')return;
     this.classList.remove('playing');
 }
 const keys=document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend',removeTransition));
-window.addEventListener('keydown',playSound);
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));//check transition for each key pressed
+window.addEventListener('keydown',playSound);//onkeydown playsound once
+window.addEventListener('keyup',function(e){
+    pressed[e.keyCode]=false;
+    //console.log(pressed[this.dataset.key]);
+});
