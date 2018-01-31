6c1dd2a6dbb2e11c427df089b79eb0f04a57dcbd
Faria Begum Riya <riya09@users.noreply.github.com>
2018-01-31 23:42:04 +0600

    added close functionality

--- LightBox/index.js
+++ LightBox/index.js
@@ -1,9 +1,14 @@
 let images = document.querySelectorAll('.col img'),
     content = document.querySelector('.content img'),
-    modal = document.querySelector('.modal');
+    modal = document.querySelector('.modal'),
+    close = document.querySelector('.close');
     
 
 images.forEach(k => k.addEventListener('click',function(){
     content.src=this.src;
     modal.classList.add('show-modal');
 }))
+
+close.addEventListener('click',function(){
+    modal.classList.remove('show-modal');
+})
