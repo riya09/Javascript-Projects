4ff4186329b72c864ea56847a19e4b77d454f15c
Faria Begum Riya <faria.ayir@gmail.com>
2019-04-14 15:19:51 +0600

    pop-quiz in react

---
+++ pop-quiz/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Homepage from './components/Homepage';
+import './style/homepage.css'
+import './style/quizpage.css'
+
+class App extends React.Component {
+  render(){
+    return(
+        <Homepage/>
+    )
+  }
+}
+
+export default App;
